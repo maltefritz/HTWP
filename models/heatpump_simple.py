32cb712a93d8de181101271180086dc43fd6c89b
jfreissmann <j.freissmann@web.de>
2022-01-18 15:34:13 +0100

    Add network, sources and sinks to simple heat pump model

--- models/heatpump_simple.py
+++ models/heatpump_simple.py
@@ -5,3 +5,23 @@ Created on Tue Jan 18 14:52:10 2022
 @author: Malte Fritz & Jonas Freißmann
 """
 
+from tespy.components import (
+    CycleCloser, Source, Sink
+    )
+from tespy.networks import Network
+
+# %% Network
+nw = Network(
+    fluids=['water', 'air', 'NH3'],
+    T_unit='C', p_unit='bar', h_unit='kJ / kg', m_unit='kg / s'
+    )
+
+# %% Components
+# Sources and Sinks
+cycle_closer = CycleCloser('Refrigerant Cycle Closer')
+
+cons_backflow = Source('Consumer Back Flow')
+cons_feedflow = Sink('Consumer Feed Flow')
+
+heatsource_feedflow = Source('Heat Source Feed Flow')
+heatsource_backflow = Sink('Heat Source Back Flow')
